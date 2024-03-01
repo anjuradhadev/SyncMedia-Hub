a071525645afbff9546ad3d067288432e7509cce
anjuradhadev <anjuradhakrishnan9048@gmail.com>
2024-03-01 18:17:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Container } from '@mui/material';
+import './App.css';
+import Navbar from './Component/Navbar/Navbar'
+
+function App() {
+  return (
+    <div >
+      <Container>
+      <Navbar/>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
